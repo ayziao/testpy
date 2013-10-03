dd89162ed0535e700c92ac1dbcd7b940f3f1e5e1
ayziao <ayziao+git@gmail.com>
2013-10-03 19:14:14 +0900

    common method muve 共通的に使いそうなものを移動(仮)

--- myapp/MyApp.py
+++ myapp/MyApp.py
@@ -1,36 +1,18 @@
-import configparser
-
-
-def load_conf():
-	conf = configparser.ConfigParser()
-	conf.read('../config/setting.ini.sample')
-	return conf
-
-
-def debug_print(environ):
-	if environ['PATH_INFO'] != '/favicon.ico':
-		from pprint import pprint
-		pprint(environ)
+import myapp.common.Utility
 
 
 def application(environ, start_response):
-	debug_print(environ)
+	myapp.common.Utility.debug_print(environ)
 
-	response = Response()
+	response = myapp.common.Utility.Response()
 	response.body = main()
 
 	start_response(response.status, response.headers)
 	return [response.body.encode("utf-8")]
 
 
-class Response():
-	status = '200 OK'
-	headers = [('Content-Type', 'text/plain; charset=utf-8')]
-	body = ''
-
-
 def main():
-	conf = load_conf()
+	conf = myapp.common.Utility.load_conf()
 	output_str = conf['section']['key']
 	output_str += ' Hello world!'
 	return output_str
--- myapp/common/Utility.py
+++ myapp/common/Utility.py
@@ -1,7 +1,26 @@
 from pprint import pprint
+import configparser
+
+
+def debug_print(environ):
+	if environ['PATH_INFO'] != '/favicon.ico':
+		from pprint import pprint
+
+		pprint(environ)
+
+
+def load_conf():
+	conf = configparser.ConfigParser()
+	conf.read('../config/setting.ini.sample')
+	return conf
+
+
+class Response():
+	status = '200 OK'
+	headers = [('Content-Type', 'text/plain; charset=utf-8')]
+	body = ''
 
 
 def pppp():
 	pprint(globals())
-
 
