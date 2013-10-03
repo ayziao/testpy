2b460b512fff9c65759da4c0f5cefe22d632b05c
ayziao <ayziao+git@gmail.com>
2013-10-03 20:14:35 +0900

    Refactoring mainの返り値はレスポンスオブジェクトにしましょうか

--- myapp/MyApp.py
+++ myapp/MyApp.py
@@ -3,19 +3,18 @@ import myapp.common.Utility
 
 def application(environ, start_response):
 	myapp.common.Utility.debug_print(environ)
-
-	response = myapp.common.Utility.Response()
-	response.body = main()
-
+	response = main()
 	start_response(response.status, response.headers)
 	return [response.body.encode("utf-8")]
 
 
 def main():
+	response = myapp.common.Utility.Response()
 	conf = myapp.common.Utility.load_conf()
 	output_str = conf['section']['key']
 	output_str += ' Hello world!'
-	return output_str
+	response.body = output_str
+	return response
 
 
 if __name__ == '__main__':
--- myapp/wsgiserver.py
+++ myapp/wsgiserver.py
@@ -10,3 +10,4 @@ def make_server():
 
 if __name__ == '__main__':
 	make_server()
+
