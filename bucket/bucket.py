e79c7d67379323f549ac2e69ff75bcdc45ee908b
ayziao <ayziao+git@gmail.com>
2013-10-03 13:22:01 +0900

    add setting file Read 設定ファイル読み込み追加

--- bucket/bucket.py
+++ bucket/bucket.py
@@ -1,3 +1,4 @@
+import configparser
 from pprint import pprint
 
 
@@ -6,7 +7,12 @@ def hoge():
 
 
 if __name__ == '__main__':
+	conf = configparser.ConfigParser()
+	conf.read('conf.ini')
+	pprint(conf['aaa']['bbb'])
 	pprint(globals())
+
+	exit()
 
 	import sys
 	from pprint import pprint
@@ -22,3 +28,47 @@ if __name__ == '__main__':
 	pprint(dir())
 
 	pprint(sys.path)
+#stdout = StringIO()
+#print("Hello world!", file=stdout)
+#print(file=stdout)
+#h = sorted(environ.items())
+#for k, v in h:
+#	print(k, '=', repr(v), file=stdout)
+#return [stdout.getvalue().encode("utf-8")]
+
+#start_response('200 OK', [('Content-type', 'text/plain')])
+#	return 'a'
+
+#return 'Hello world'
+
+
+#def run(app):
+#	server = simple_server.make_server('', 8080, app)
+#	server.serve_forever()
+#run(application)
+
+
+
+
+#x = common.Utility
+
+
+#def ppp():
+#	common.Utility.pppp()
+#	pprint(globals())
+#
+#
+#def hello(environ, start_response):
+#	u'''
+#	Hello, world
+#	'''
+#
+#	#
+#	start_response('200 OK', [('Content-Type', 'text/plain')])
+#
+#	#
+#	return ['Hello, World']
+#
+#
+
+#ppp()
--- myapp/MyApp.py
+++ myapp/MyApp.py
@@ -1,3 +1,4 @@
+import configparser
 from pprint import pprint
 from wsgiref import simple_server
 
@@ -5,60 +6,21 @@ from wsgiref import simple_server
 
 
 def application(environ, start_response):
-	#stdout = StringIO()
-	#print("Hello world!", file=stdout)
-	#print(file=stdout)
-	#h = sorted(environ.items())
-	#for k, v in h:
-	#	print(k, '=', repr(v), file=stdout)
-	start_response("200 OK", [('Content-Type', 'text/plain; charset=utf-8')])
+	conf = configparser.ConfigParser()
+	conf.read('../config/setting.ini.sample')
 
-	if (environ['PATH_INFO'] != '/favicon.ico'):
+	str = conf['section']['key']
+
+	start_response("200 OK", [('Content-Type', 'text/plain; charset=utf-8')])
+	if environ['PATH_INFO'] != '/favicon.ico':
 		pprint(environ)
 
-	return ["Hello world!".encode("utf-8")]
-
-#return [stdout.getvalue().encode("utf-8")]
-
-#start_response('200 OK', [('Content-type', 'text/plain')])
-#	return 'a'
-
-#return 'Hello world'
-
-
-#def run(app):
-#	server = simple_server.make_server('', 8080, app)
-#	server.serve_forever()
+	str += " Hello world!"
+	return [str.encode("utf-8")]
 
 
 if __name__ == '__main__':
 	#server = simple_server.make_server('localhost', 8080, application)
 	server = simple_server.make_server('', 8080, application)
 	server.serve_forever()
-#run(application)
 
-
-
-
-#x = common.Utility
-
-
-#def ppp():
-#	common.Utility.pppp()
-#	pprint(globals())
-#
-#
-#def hello(environ, start_response):
-#	u'''
-#	Hello, world
-#	'''
-#
-#	#
-#	start_response('200 OK', [('Content-Type', 'text/plain')])
-#
-#	#
-#	return ['Hello, World']
-#
-#
-
-#ppp()
