a4320d9492d4fe246591ec59b2f99e436d1347c6
ayziao <ayziao+git@gmail.com>
2013-10-09 17:15:43 +0900

    Merge branch 'master' of https://github.com/ayziao/testpy
    Conflicts:
    	myapp/common/request.py

--- myapp/common/request.py
+++ myapp/common/request.py
@@ -8,13 +8,13 @@
 class Request():
 	def __init__(self):
 		self.path = ''
-	extension = 'html' # 出力形式(拡張子)
-		self.command = '' # ユーザーコマンド
-		self.parameter = '' # ユーザーパラメータ
-		self.input = '' # ユーザ入力(ファイルとか プロパティはガワだけで処理を入れる コールバック関数？)
-		self.systemCommand = '' # システムコマンド
-		self.systemParameter = '' # システムパラメータ
-		self.systemInput = '' # システム入力(コールバック関数？)
+		self.extension = 'html'  # 出力形式(拡張子)
+		self.command = ''  # ユーザーコマンド
+		self.parameter = ''  # ユーザーパラメータ
+		self.input = ''  # ユーザ入力(ファイルとか プロパティはガワだけで処理を入れる コールバック関数？)
+		self.system_command = ''  # システムコマンド
+		self.system_parameter = ''  # システムパラメータ
+		self.system_input = ''  # システム入力(コールバック関数？)
 		self.paging = ''
-		self.controllerClassName = ''
-		self.methodName = ''
+		self.controller_class_name = ''
+		self.method_name = ''
