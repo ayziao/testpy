d0a42f8b25c88edcb60ce2b6b1f962233385efdf
ayziao <ayziao+git@gmail.com>
2013-10-22 19:43:09 +0900

    debug class -> module
    とりあえずクラスからモジュールにした

---
+++ myapp/controller/account.py
@@ -0,0 +1,26 @@
+"""
+# myapp.controller.account
+"""
+
+
+class Account():
+	"""
+	# アカウント
+	"""
+
+	def __init__(self):
+		pass
+
+	def signup(self):
+		pass
+
+	def login(self):
+		pass
+
+
+
+		#アカウント/作成
+		#アカウント/ログイン
+		#アカウント/
+		#アカウント/
+		#アカウント/
