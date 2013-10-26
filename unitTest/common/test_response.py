864d42704feffaf5797279f7207997b52291622c
ayziao <ayziao+git@gmail.com>
2013-10-27 03:31:17 +0900

    test response.

--- unitTest/common/test_response.py
+++ unitTest/common/test_response.py
@@ -1,23 +1,37 @@
 import unittest
-from myapp.common.response import Response
+from myapp.common import response
 
 
 class TestResponse(unittest.TestCase):
 	def test_init(self):
-		res = Response()
+		res = response.Response()
 		self.assertEqual(res.status, '200 OK')
 
-
 	def test_status_code_404(self):
-		res = Response()
+		res = response.Response()
 		res.status_code = 404
 		self.assertEqual(res.status, '404 Not Found')
 
-
 	def test_status_code_200(self):
-		res = Response()
+		res = response.Response()
 		res.status_code = 200
 		self.assertEqual(res.status, '200 OK')
+
+	def test_get_instance(self):
+		res = response.get_instance()
+		self.assertEqual(res.status, '200 OK')
+
+	def test_create_instance(self):
+		res = response.create_instance()
+		self.assertEqual(res.status, '200 OK')
+
+	def test_pop_instance(self):
+		res = response.pop_instance()
+		res = response.pop_instance()
+		res = response.pop_instance()
+		res = response.pop_instance()
+		res = response.pop_instance()
+		self.assertIsNone(res)
 
 
 if __name__ == '__main__':
