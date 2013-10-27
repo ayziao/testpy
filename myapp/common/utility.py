ff082ad621324d7ac1992e7125aebef82b99cea9
ayziao <ayziao+git@gmail.com>
2013-10-27 20:02:33 +0900

    utility recursive_directory

--- myapp/common/utility.py
+++ myapp/common/utility.py
@@ -5,6 +5,7 @@
 # Pythonのめんどくさいとこ簡単にするのとか
 # アプリケーションに依存しないもの
 """
+import os
 
 
 def import_(module_name: str) -> "module":
@@ -23,3 +24,18 @@ def import_(module_name: str) -> "module":
 
 	except (ImportError, AttributeError):
 		return None
+
+
+def recursive_directory(dir_: str, func: 'function(file_path: str)') -> None:
+	"""
+	#translationME 再帰的にディレクトリ内のファイルに処理を行う
+	@param dir_: ディレクトリパス文字列
+	@param func: ファイルパス文字列を引数に持つ関数オブジェクト
+	"""
+	for basename in os.listdir(dir_):
+		path = dir_ + '/' + basename
+		if not basename.startswith('_'):  # FIXME 処理対象外ファイル、ディレクトリ名のチェック関数も受け取るか
+			if os.path.isdir(path):
+				recursive_directory(path, func)
+			elif os.path.isfile(path):
+				func(path)
--- unitTest/all_test.py
+++ unitTest/all_test.py
@@ -8,26 +8,24 @@ path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(path.rstrip('/unitTest'))
 
 from myapp.common import settings
+from myapp.common import utility
 
 settings.set_config_path(path + '/config/')
 settings.setting_encode()
 
 
-def load_testpy(path, suite, loader):
-	for file in os.listdir(path):
-		path_file = path + '/' + file
-		if os.path.isdir(path_file) and not file.startswith('_'):
-			load_testpy(path_file, suite, loader)
-		elif os.path.isfile(path_file) and file.startswith('test_') and file.endswith('.py'):
-			mod = imp.load_source(os.path.splitext(file)[0], path_file)
-			suite.addTest(loader.loadTestsFromModule(mod))
-	pass
-
-
 def run():
 	suite = unittest.TestSuite()
 	loader = unittest.TestLoader()
-	load_testpy(os.path.curdir, suite, loader)
+
+	def addTest(path):
+		file = os.path.basename(path)
+		if file.startswith('test_') and file.endswith('.py'):
+			mod = imp.load_source(os.path.splitext(file)[0], path)
+			suite.addTest(loader.loadTestsFromModule(mod))
+
+	utility.recursive_directory(os.path.curdir, addTest)
+
 	unittest.TextTestRunner(verbosity=2).run(suite)
 
 
