15ea0066b5b5a93b4380f4bea42474c2ca02a5c7
ayziao <ayziao+git@gmail.com>
2013-11-05 13:36:37 +0900

    common.settings unit test.

--- tests/common/test_settings.py
+++ tests/common/test_settings.py
@@ -13,12 +13,19 @@ from myapp.common import settings
 
 class TestSettings(unittest.TestCase):
 	def test_get_ini(self):
+		#セクション指定
 		settings.set_config_path(path + '/../config/')
 		conf = settings.get_ini('test')
 		self.assertEqual(conf['test'], 'test')
+
+		#セクション指定なし
 		conf = settings.get_ini()
 		conf2 = conf['test']
 		self.assertEqual(conf2['test'], 'test')
+
+		#存在しないセクション
+		conf = settings.get_ini('nothing_section')
+		self.assertIsNone(conf)
 
 	def test_get_ini_path(self):
 		path_ = settings._get_ini_path()
