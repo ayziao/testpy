b8ca510a008aa23ab1563cfd3f5657800b61f281
ayziao <ayziao+git@gmail.com>
2013-11-05 09:25:53 +0900

    common.settings unit test.

--- tests/common/test_settings.py
+++ tests/common/test_settings.py
@@ -16,6 +16,9 @@ class TestSettings(unittest.TestCase):
 		settings.set_config_path(path + '/../config/')
 		conf = settings.get_ini('test')
 		self.assertEqual(conf['test'], 'test')
+		conf = settings.get_ini()
+		conf2 = conf['test']
+		self.assertEqual(conf2['test'], 'test')
 
 	def test_get_ini_path(self):
 		path_ = settings._get_ini_path()
