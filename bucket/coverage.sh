e7a28c2b1539cf0684f20f4908e40cc796802d7b
ayziao <ayziao+git@gmail.com>
2013-11-08 13:14:02 +0900

    model.basedata unit test.

---
+++ bucket/coverage.sh
@@ -0,0 +1,4 @@
+#mac用
+/Library/Frameworks/Python.framework/Versions/3.3/bin/coverage run --source=myapp tests/all_test.py
+/Library/Frameworks/Python.framework/Versions/3.3/bin/coverage report -m
+/Library/Frameworks/Python.framework/Versions/3.3/bin/coverage html
