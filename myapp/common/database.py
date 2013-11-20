b08b5c10f666745b93e04c7519e20abb16f50536
ayziao <ayziao+git@gmail.com>
2013-11-21 03:22:16 +0900

    common.database property comment out
    型が明確になれば補完効くようになるかなーと思ったけど プロパティクラス型になるくさいので必要になるまでモジュール変数に直アクセスで

--- myapp/common/database.py
+++ myapp/common/database.py
@@ -7,30 +7,26 @@ import sqlite3
 #	コネクションを保持 sqlite PostgreSQL
 #	複数のコネクション持つ マスタースレーブ 垂直分割
 #	エンティティ毎のコネクション管理
-_connection = None
-_sqlite_connection = None
+connection = None
 
-__status_code = None
-
-
-@property
-def connection() -> sqlite3.Connection:
-	"""
-	test
-	@return:
-	"""
-	return _connection
-
-
-@connection.setter
-def connection(val: sqlite3.Connection):
-	"""
-	test
-	@param val:
-	@return:
-	"""
-	global _connection
-	_connection = val
+#@property
+#def connection() -> sqlite3.Connection:
+#	"""
+#	test
+#	@return:
+#	"""
+#	return _connection
+#
+#
+#@connection.setter
+#def connection(val: sqlite3.Connection):
+#	"""
+#	test
+#	@param val:
+#	@return:
+#	"""
+#	global _connection
+#	_connection = val
 
 
 #entityかデータベース情報をもとにコネクション返す
@@ -63,15 +59,15 @@ def insert(entity):
 
 def update(entity):
 	#PENDING プライマリキー変更するときの処理考える
-	set = ''
+	set_ = ''
 	vals = list()
 	for k, v in entity.__dict__.items():
 		if k[0] != '_':
-			set += '`' + k + '` = ? ,'
+			set_ += '`' + k + '` = ? ,'
 			vals.append(v)
 	vals.append(entity.__dict__[entity._key])  # PENDING 複合キーの時どうするか
 	name = entity.__class__.__name__
-	sql = "UPDATE %s SET %s WHERE %s = ?" % (name, set[0:-1], entity._key)
+	sql = "UPDATE %s SET %s WHERE %s = ?" % (name, set_[0:-1], entity._key)
 	connection.execute(sql, tuple(vals))
 
 
@@ -82,17 +78,17 @@ def select(entity, parameter: list):
 	@param parameter: [(key:val),(key:val)...]
 	"""
 	where = ''
-	vals = list()
+	val_list = list()
 	for item in parameter:
 		if item[0][0] != '_':
 			where += '`' + item[0] + '` = ? AND ,'
-			vals.append(item[1])
+			val_list.append(item[1])
 
 	name = entity.__class__.__name__
 	sql = "SELECT * FROM `%s` WHERE %s LIMIT 1" % (name, where[0:-5])
 	connection.row_factory = sqlite3.Row
 	c = connection.cursor()
-	c.execute(sql, tuple(vals))
+	c.execute(sql, tuple(val_list))
 	row = c.fetchone()
 	for k in row.keys():
 		entity.__dict__[k] = row[k]
