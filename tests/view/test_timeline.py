bead359cee13e288d9d47dfde61abb9015180141
ayziao <ayziao+git@gmail.com>
2013-11-30 21:56:58 +0900

    TestVewTimeline

--- tests/view/test_timeline.py
+++ tests/view/test_timeline.py
@@ -8,6 +8,11 @@ class TestVewTimeline(unittest.TestCase):
 	def test_view(self):
 		req = Request()
 		view = TimeLine(req)
+		res = view.view(None)
+		self.assertEqual(res.body[0:17], "time line") # TODO もっとマシに
+
+	def test_view_html(self):
+		req = Request()
+		view = TimeLine(req)
 		html = view._view_html(None)
 		self.assertEqual(html[0:17], "<html>\n\t<head>\n\t\t") # TODO もっとマシに
-		pass
