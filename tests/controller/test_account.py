1069830ec11437066828d1379f4b07ef2f1eb00e
ayziao <ayziao+git@gmail.com>
2013-12-24 16:16:28 +0900

    Never import the module-under-test at test module scope.

--- tests/controller/test_account.py
+++ tests/controller/test_account.py
@@ -3,28 +3,37 @@
 """
 import unittest
 
-from myapp.controller.account import Account
-
 
 class TestAccount(unittest.TestCase):
+	def _get_target_class(self):
+		from myapp.controller.account import Account
+
+		return Account
+
+	def _get_target_class_instance(self):
+		class_ = self._get_target_class()
+		return class_()
+
 	def test_init(self):
-		account = Account()
-		self.assertIsInstance(account, Account)
+		account = self._get_target_class_instance()
+		self.assertIsInstance(account, self._get_target_class())
+
+	# PENDING アサート
 
 	def test_signup(self):
-		account = Account()
+		account = self._get_target_class_instance()
 		account.signup()
 		self.assertTrue(True)
 	# PENDING アサート
 
 	def test_login(self):
-		account = Account()
+		account = self._get_target_class_instance()
 		account.login()
 		self.assertTrue(True)
 	# PENDING アサート
 
 	def test_loguout(self):
-		account = Account()
+		account = self._get_target_class_instance()
 		account.logout()
 		self.assertTrue(True)
 
