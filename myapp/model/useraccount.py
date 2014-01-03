6901c25056664d07148d11b5327a83d61d8be1e2
ayziao <ayziao+git@gmail.com>
2014-01-03 23:58:36 +0900

    todo

--- myapp/model/useraccount.py
+++ myapp/model/useraccount.py
@@ -2,6 +2,19 @@
 # myapp.model.useraccount
 """
 
+#UUID
+#パスワード
+#メールアドレス
+#秘密鍵
+#更新日時
+#作成日時
+
+#identifier
+#mail address
+#password hash
+#password salt
+#signup date
+#lastLogin date
 
 class UserAccount():
 	pass
