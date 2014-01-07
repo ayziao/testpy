7280939a084dd381cb4655114d3219cd98d3a3b1
ayziao <ayziao+git@gmail.com>
2014-01-08 04:12:33 +0900

    merge fix

--- myapp/view/timeline.py
+++ myapp/view/timeline.py
@@ -45,11 +45,10 @@ class TimeLine:
 		for num, model in enumerate(model_list):
 			if num > 0: # 2件以上あるとき改行
 				mmm += '<br/>'
-			mmm += str(num) + ' ' + model.id + ' ' + model.title + ' ' + model.tag + ' ' + model.body
+			mmm += str(
+				num) + ' <a href="/' + model.id + '">' + model.id + '</a> ' + model.title + ' ' + model.tag + ' ' + model.body
 			if model.datetime is not None: # FIXME テストで作るデータに日時が入らない臭いので
 				mmm += ' ' + model.datetime
-			mmm += str(
-				num) + ' <a href="/' + model.id + '">' + model.id + '</a> ' + model.title + ' ' + model.tag + ' ' + model.body + ' ' + model.datetime
 		# mmm += str(num) + ' ' + model.id + ' ' + model.title + ' ' + model.tag + ' ' + model.body
 		mmm += '</div>'
 
