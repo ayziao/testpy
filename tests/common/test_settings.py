3b2775c7c146ff5062ad86a2129258c05d49dcd7
ayziao <ayziao+git@gmail.com>
2014-01-15 20:31:11 +0900

    divide test method

--- tests/common/test_settings.py
+++ tests/common/test_settings.py
@@ -13,21 +13,21 @@ from myapp.common import settings
 
 class TestSettings(unittest.TestCase):
 	def test_get_ini(self):
-		#セクション指定
-		settings.set_config_path(path + '/../config/')
-		conf = settings.get_ini('test')
-		self.assertEqual(conf['test'], 'test')
-
 		#セクション指定なし
 		conf = settings.get_ini()
 		conf2 = conf['test']
 		self.assertEqual(conf2['test'], 'test')
 
+	def test_get_ini_section(self):
+		#セクション指定
+		settings.set_config_path(path + '/../config/')
+		conf = settings.get_ini('test')
+		self.assertEqual(conf['test'], 'test')
+
+	def test_get_ini_section_nothing_section(self):
 		#存在しないセクション
 		conf = settings.get_ini('nothing_section')
 		self.assertIsNone(conf)
-
-	#TODO method分ける
 
 	def test_get_ini_path(self):
 		path_ = settings._get_ini_path()
