2f834fd906a6e721ceab382b454bfdf222547778
ayziao <ayziao+git@gmail.com>
2014-01-21 22:16:00 +0900

    UserAccount

--- myapp/model/useraccount.py
+++ myapp/model/useraccount.py
@@ -2,6 +2,8 @@
 # myapp.model.useraccount
 """
 
+from myapp.common import database
+
 #UUID
 #パスワード
 #メールアドレス
@@ -17,5 +19,38 @@
 #lastLogin date
 
 class UserAccount():
-	pass
+	"""
+	ユーザーアカウント
+	"""
 
+	@staticmethod
+	def create_table():
+		sql = """
+		create table UserAccount (
+			identifier varchar(20) PRIMARY KEY,
+			mail varchar(500),
+			password varchar(500),
+			signup text,
+			last_login text
+		);
+		"""
+		database.execute(sql)  # PENDING 何処のデータベースに作るか指定出来るように
+
+	def __init__(self, identifier: str=None) -> None:
+		self.identifier = ''
+		self.mail = ''
+		self.password = ''
+		self.signup = None
+		self.last_login = None
+		self._key = 'identifier'
+		if identifier:
+			self.load_by_identifier(identifier)
+
+	def load_by_identifier(self, identifier: str) -> None:
+		prm = [('identifier', identifier)]
+		database.select(self, prm)
+		return self.identifier
+
+	@classmethod
+	def commit(cls):
+		database.connection.commit()
---
+++ tests/model/test_useraccount.py
@@ -0,0 +1,38 @@
+import unittest
+
+from myapp.common import database
+from myapp.model.useraccount import UserAccount
+
+
+class TestUserAccount(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		#database.get_connection('/var/tmp/test.sqlite3db')  # PENDING 引数仮
+		database.get_connection(':memory:')  # PENDING 引数仮
+		UserAccount.create_table()
+
+		#テストデータインサート
+		database.connection.executemany("insert into UserAccount values (?, ?, ?, ?, ?)", [
+			("test", 'hoge@piyo.com', 'aaaa', '2012-12-31 23:59:59.123456', '2012-12-31 23:59:59.123456')])
+
+
+	@classmethod
+	def tearDownClass(cls):
+		UserAccount.commit()
+		database.connection.close()
+
+	@classmethod
+	def tearDownClass(cls):
+		database.connection.commit()
+		database.connection.close()
+
+	def setUp(self):
+		self.obj = UserAccount()
+
+	def test_init(self):
+		obj = UserAccount()
+		self.assertIsInstance(obj, UserAccount)
+		self.assertEqual(obj.identifier, '')
+
+		obj = UserAccount('test')
+		self.assertEqual(obj.identifier, 'test')
